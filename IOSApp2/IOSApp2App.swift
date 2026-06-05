9b9e3a08150de7a1d411554a23d5565d16bf5705
FoziaAkhtar <fozia.shah0001@gmail.com>
2026-06-04 20:35:10 -0400

    Initial Commit

---
+++ IOSApp2/IOSApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  IOSApp2App.swift
+//  IOSApp2
+//
+//  Created by Fozia on 2026-06-04.
+//
+
+import SwiftUI
+
+@main
+struct IOSApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
